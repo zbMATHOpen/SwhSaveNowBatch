7f5563098e7a920ed2169adc8721c2ee3345bc33
Moritz Schubotz (physikerwelt) <wiki@physikerwelt.de>
2019-11-27 15:15:30 +0100

    fix: problem in template
    On windows the apostrophe in the file pattern causes
    the following error
    ```
    No files matching the pattern "'./src/**/*.{js,ts,tsx}'" were found.
    ```
    Removing the apostrophe solved the problem.

--- src/__tests__/greet.test.ts
+++ src/__tests__/greet.test.ts
@@ -1,5 +1,7 @@
-import { greet } from '..'
+import { greet, swh } from '..'
 
 test('My Greeter', () => {
   expect(greet('Carl')).toBe('Hello Carl')
 })
+
+test('Get swh list', async () => swh())
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
+import { Response } from 'node-fetch'
+import csv = require('fast-csv')
+import CsvParserStream from 'fast-csv/build/src/parser/CsvParserStream'
+
+import fetch from 'node-fetch'
+
+function parseCSV(res: Response): Promise<CsvParserStream> {
+  return new Promise((resolve) => {
+    const stream = res.body.pipe(csv.parse())
+    stream
+      .on('error', /* istanbul ignore next */ (error: Error) => console.error(error))
+      .on('data', (row: string) => console.log(`ROW=${JSON.stringify(row)}`))
+      .on('end', (rowCount: number) => {
+        console.log(`Parsed ${rowCount} rows`)
+        resolve(stream)
+      })
+  })
+}
+
 export const greet = (name: string) => `Hello ${name}`
+
+export const swh = () =>
+  fetch('http://swmath.org/SWH/')
+    .then(parseCSV)
+    .then((stream: CsvParserStream) => stream.end())
